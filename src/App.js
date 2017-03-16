9c8d138920538b51ca882802fe1f3f36ba274886
Ranjeev Atwal <ranjeev.atwal@gmail.com>
2017-03-16 15:45:32 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Babylon Shopping Basket</h2>
+        </div>
+        <p className="App-intro">
+          Home
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
